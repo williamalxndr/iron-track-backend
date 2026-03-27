b01354ce285a6e0dfba84d49f7f70d10b45d704a
William Alexander <williamax2005@gmail.com>
2026-03-27 13:41:14 +0700

    fix(backend): add coverage to requirements.txt for CI

--- config/settings.py
+++ config/settings.py
@@ -91,6 +91,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 # Simple JWT
